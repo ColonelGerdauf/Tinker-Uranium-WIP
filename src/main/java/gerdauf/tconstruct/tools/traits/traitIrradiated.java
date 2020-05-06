7fea74ff74e3097920cd692bae803b4d431ad3e5
ColonelGerdauf <ColonelGerdauf@users.noreply.github.com>
2020-05-05 23:07:17 -0400

    improved code modularization

--- src/main/java/gerdauf/tconstruct/tools/traits/traitIrradiated.java
+++ src/main/java/gerdauf/tconstruct/tools/traits/traitIrradiated.java
@@ -5,14 +5,15 @@ import net.minecraft.init.*;
 import net.minecraft.item.*;
 import net.minecraft.world.*;
 import slimeknights.tconstruct.library.traits.*;
-import slimeknights.tconstruct.library.utils.ToolHelper;
-
-private byte dmgcounter = 0; 
-private short effectcounter1 = 0; 
-private short effectcounter2 = 0; 
+import slimeknights.tconstruct.library.utils.*;
 
 // apply to uranium sets or pieces
 public class Irradiation extends AbstractTrait {
+	
+	private byte dmgcounter = 0; 
+	private short effectcounter1 = 0; 
+	private short effectcounter2 = 0; 
+	
 	public static final Irradiation irradiation = new Irradiation();
 	
 	public Irradiation() {
@@ -22,8 +23,8 @@ public class Irradiation extends AbstractTrait {
 	@Override
 	public void onUpdate(ItemStack tool, World world, Entity entity, int itemSlot, boolean isSelected) {
 		
-		double curDurability = ToolHelper.getCurrentDurability(tool);		
-		double maxDurability = ToolHelper.getMaxDurability(tool);
+		int curDurability = ToolHelper.getCurrentDurability(tool);		
+		int maxDurability = ToolHelper.getMaxDurability(tool);
 
 		double DurabilityRemaining = curDurability / maxDurability;
 
@@ -35,38 +36,40 @@ public class Irradiation extends AbstractTrait {
 		else if (DurabilityRemaining < 0.25f) {
 			supercritical_ii(tool);
 		}
-	}
-  
-	public void supercritical(ItemStack tool) {
-		// inflict the equivalent of Poison I to the wearers of the armor
-		if (effectcounter1 >= 25) {
-			//super.onPlayerHurt(1);
-			effectcounter1 = 0;
-		}
-		else
-			effectcounter1++;
-		// inflict the equivalent of Poison I to people within 0 blocks of proximity to the wearers of the armor for 2s
+	}	
+	private void durabilityDecay(byte dmg, byte sec) {
+		short tickTimer = (sec * 20) / dmg;
 		
-		// drop durability by 1 point per 120 ticks		
-		if (dmgCounter >= 120) {
-			//super.onToolDamage(1);
-			dmgCounter = 0;
-		}
-		else
-			dmgCounter++;
+		if (!TagUtil.getTagSafe(tool).getBoolean(ModReinforced.TAG_UNBREAKABLE)
+			if (dmgCounter >= tickTimer) {
+    		tool.setItemDamage(tool.getItemDamage() + 1);
+				dmgCounter = 0;
+			}
+			else
+				dmgCounter++;		    
+	} 
+	private void hurtUser(boolean wither, byte dmg) {
+	}
+	private void hurtOther(boolean wither, byte blockProx, byte dmg, byte sec) {
+	}
+	public void supercritical(ItemStack tool) {
+		// inflict the equivalent of Poison I to the users
+		hurtUser(false,1);
+		// inflict the equivalent of Poison I to people within 0 blocks of proximity to the users for 2s
+		hurtOther(false,0,1,2);
+		// drop durability by 1 point per 6 seconds
+		durabilityDecay(1,6);
 	}
 	public void supercritical_ii(ItemStack tool) {  
-		// inflict the equivalent of Wither II to the wearers of the armor
-		// inflict the equivalent of Poison I to people within 5 blocks of proximity to the wearers of the armor for 2s
-		// inflict the equivalent of Poison II to people within 2 blocks of proximity to the wearers of the armor for 5s
-		// inflict the equivalent of Poison IV to people within 0 blocks of proximity to the wearers of the armor for 25s
-		
-		// drop durability by 1 point per 20 ticks
-		if (dmgCounter >= 20) {
-			//super.onToolDamage(1);
-			dmgCounter = 0;
-		}
-		else
-			dmgCounter++;
+		// inflict the equivalent of Wither II to the users		
+		hurtUser(true,2);
+		// inflict the equivalent of Poison I to people within 5 blocks of proximity to the users for 2s
+		hurtOther(false,5,1,2);
+		// inflict the equivalent of Poison II to people within 2 blocks of proximity to the users for 5s
+		hurtOther(false,2,1,5);
+		// inflict the equivalent of Poison IV to people within 0 blocks of proximity to the users for 25s
+		hurtOther(false,0,1,25);
+		// drop durability by 1 point per second
+		durabilityDecay(1,1);
 	}
 }
